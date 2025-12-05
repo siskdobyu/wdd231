523803765dc20d16e1a141cd7ecb52dec1789b79
siskdobyu <karinadolojan@gmail.com>
2025-12-05 18:30:13 +0800

    with updates

---
+++ project/scripts/script.js
@@ -0,0 +1,4 @@
+// current year in footer
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
