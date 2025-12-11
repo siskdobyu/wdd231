7d49b8866abe0d7ebe84f228be46ae23763e70c5
siskdobyu <karinadolojan@gmail.com>
2025-12-12 02:36:14 +0800

    with updates

--- finalproject/scripts/script.js
+++ finalproject/scripts/script.js
@@ -1,4 +1,46 @@
-// current year in footer
-document.querySelector("#currentyear").textContent = new Date().getFullYear();
+// ---------------------
+// MOBILE MENU
+// ---------------------
+const menuBtn = document.querySelector("#menuBtn");
+const navMenu = document.querySelector("#navMenu");
 
-document.getElementById("lastModified").innerHTML = document.lastModified;
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("hidden");
+});
+
+// ---------------------
+// FOOTER DATES
+// ---------------------
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+/// === Fetch Members ===
+const directory = document.querySelector("#directory");
+
+async function loadMembers() {
+    const response = await fetch("data/emergencycontacts.json");
+    const members = await response.json();
+
+    displayMembers(members);
+}
+
+function displayMembers(members) {
+    directory.innerHTML = "";
+
+    members.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="images/${member.image}" alt="${member.name} logo">
+            <h3>${member.name}</h3>
+            <p>${member.phone}</p>
+            <a href="${member.link}" target="_blank">Link</a>
+        `;
+
+        directory.appendChild(card);
+    });
+}
+
+loadMembers();
+
